65ec9568a5b4e64ad46754b6eacefa31d93560e5
NakamuraFumiya <fumiya3691@gmail.com>
2023-05-10 21:58:55 +0900

    done

--- abc085/a/main.go
+++ abc085/a/main.go
@@ -5,13 +5,5 @@ import "fmt"
 func main() {
 	var S string
 	fmt.Scan(&S)
-	var s string
-	for i := range S {
-		if i == 3 {
-			s = s + "8"
-		} else {
-			s = s + fmt.Sprintf("%c", S[i])
-		}
-	}
-	fmt.Println(s)
+	fmt.Println("2018" + S[4:])
 }
