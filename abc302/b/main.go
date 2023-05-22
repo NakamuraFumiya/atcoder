d08b7d1fb76605a387134897fa6ff2053e8d916c
vs-fumiya-nakamura <f.nakamura@v-standard.com>
2023-05-22 14:09:48 +0900

    tweak

--- abc302/b/main.go
+++ abc302/b/main.go
@@ -20,9 +20,35 @@ func main() {
 		fmt.Fscan(r, &strSlice[i])
 	}
 
+	T := "snuke"
+	// i, jがどのくらい変化するのかのテーブルを作る
+	di := []int{-1, -1, -1, 0, 0, 1, 1, 1}
+	dj := []int{-1, 0, 1, -1, 1, -1, 0, 1}
 	for si := 0; si < H; si++ {
 		for sj := 0; sj < W; sj++ {
-
+			i, j := si, sj
+			// 8方向で探索
+			for k := 0; k < 8; k++ {
+				for l := 0; l < 5; l++ {
+					if i < 0 || j < 0 || i >= H || j >= W {
+						break
+					}
+					if string(strSlice[i][j]) != string(T[l]) {
+						break
+					}
+					if k == 4 {
+						for m := 0; m < 5; m++ {
+							i, j = si, sj
+							fmt.Printf("%d %d\n", i+1, j+1)
+							i += di[k]
+							j += dj[k]
+						}
+					}
+					// TODO: ()で括らなくて良い？
+					i += di[k]
+					j += dj[k]
+				}
+			}
 		}
 	}
 }
